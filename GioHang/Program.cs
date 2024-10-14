6b6aab5360b15608ecb47492ca0d9882b1dd32ec
LeeHuy18 <levonhathuy12b5@gmail.com>
2024-10-14 23:26:42 +0700

    first commit

---
+++ GioHang/Program.cs
@@ -0,0 +1,14 @@
+﻿using GioHang;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using GioHang.Services; // Đảm bảo namespace chính xác
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Đăng ký dịch vụ CartService
+builder.Services.AddScoped<CartServices>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ GioHang/Services/CartServices.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+namespace GioHang.Services
+{
+    public class CartServices
+    {
+        public List<string> Items { get; private set; } = new List<string>();
+
+        public void AddItem(string item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(string item)
+        {
+            Items.Remove(item);
+        }
+
+        public void ClearCart()
+        {
+            Items.Clear();
+        }
+
+        public void UpdateItem(int index, string newItem)
+        {
+            if (index >= 0 && index < Items.Count)
+            {
+                Items[index] = newItem;
+            }
+        }
+    }
+}
